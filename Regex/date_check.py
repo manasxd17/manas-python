4dc105331918d42494572df77a54760dd651d481
Manas Maheswari <62172748+manasxd17@users.noreply.github.com>
2021-07-31 16:34:42 +0530

    Delete date_check.py

--- Regex/date_check.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def dateCheck(Date):
-    if(re.match(r"\D+\s+0[0-9]{1,2}$",Date)):   #Working for month and date only
-        return f"Correct Date"
-    else:
-        return f"Incorrect Date"
-
-print(dateCheck(input()))
